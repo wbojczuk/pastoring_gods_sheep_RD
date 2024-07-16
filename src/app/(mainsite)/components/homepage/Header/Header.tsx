1e1fd61573bc69191cd4cea3f9d7483d014faa17
William Bojczuk <williambojczuk@gmail.com>
2024-07-15 23:06:16 -0400

    updated

--- src/app/(mainsite)/components/homepage/Header/Header.tsx
+++ src/app/(mainsite)/components/homepage/Header/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
         </div>
 
         <div className={styles.content}>
-            <p>A Dynamic Christian Talk Show at Charity Church. Delve into Deep Spiritual Insights, Thought-Provoking Discussions, and Inspirational Messages, Anchored in the Timeless Wisdom of the Bible. Join Us on a Journey of Faith, as We Shepherd God's Flock with Love, Compassion, and Unwavering Devotion.</p>
+            <p>A Dynamic Christian Talk Show on TMN. Delve into Deep Spiritual Insights, Thought-Provoking Discussions, and Inspirational Messages, Anchored in the Timeless Wisdom of the Bible. Join Us on a Journey of Faith, as We Shepherd God's Flock with Love, Compassion, and Unwavering Devotion.</p>
 
             <div className={styles.buttonWrapper}>
                 <Link className="main-link" href="/visit">Join Us</Link>
